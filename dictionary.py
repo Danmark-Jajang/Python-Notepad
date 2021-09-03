23761731649ec2caa6b6f38b66ce8ed5eb68cb72
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-09-04 01:59:22 +0900

    dictionary
    dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+dict_a = {
+    "name" : "KMS",
+    "age" : 21,
+    "live" : "Osan",
+    "job" : "student",
+    "sex" : "male"
+}
+
+print(dict_a["job"])
+
+dict_a["weight"] = 80
+print(dict_a)
+
+del dict_a["live"]
+print(dict_a)
