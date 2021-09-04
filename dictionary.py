ee07fe0d8734eb0cecb549f28a12f952881f2937
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-09-05 00:21:43 +0900

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -13,3 +13,13 @@ print(dict_a)
 
 del dict_a["live"]
 print(dict_a)
+
+value = dict_a.get("no")
+print("value:", value)
+if value==None:
+    print("You try accessing no exist key value")
+
+for i in dict_a:
+    print(dict_a.get(i))
+
+print(type({}),"is dict")
