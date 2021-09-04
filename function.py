14c266eac8b6b88bacfd06fccf0541c2aca5a624
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-09-05 00:21:48 +0900

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+def print_3_time():
+    print("hello")
+    print("hello")
+    print("hello")
+
+def print_n_time(value, n):
+    for i in range(5):
+        print(value)
+
+print_3_time()
+print_n_time("HI^^", 5)
